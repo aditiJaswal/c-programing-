af399e9abf4792e12ee7ea50b1e2ff1e6ced549d
aditi jaswal <aditijaswal341@gmail.com>
2024-08-20 21:44:15 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum,sub,mult,div;
+    cout<<"enter two number:";
+    cin>>a>>b;
+    sum=a+b;
+    sub=a-b;
+    mult=a*b;
+    div=a/b;
+    cout<<"the sum is:"<<sum<<endl;
+    cout<<sub<<endl;
+    cout<<mult<<endl;
+    cout<<div<<endl;
+    
+}
