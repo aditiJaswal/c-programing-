cc330ead92041cff849ad4c8cae25cdd5190e18b
aditi jaswal <aditijaswal341@gmail.com>
2024-08-22 22:37:37 +0530

    Add files via upload

---
+++ tut.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =14;
+    int b =17;
+    cout<<"this is tutorial 1. here the value of a is "<<a<<". the value of b is "<< b;
+    return 0;
+
+}
