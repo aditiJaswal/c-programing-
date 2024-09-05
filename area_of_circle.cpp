749ccd3162f721e681b99da3484473515c455f72
aditi jaswal <aditijaswal341@gmail.com>
2024-09-05 21:10:18 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int r;
+    float c ;
+    cout <<"enter number:";
+    cin>> r;
+    c = 3.14*r*r;
+    cout<<"area is : " << c;
+    return 0;
+
+    }
